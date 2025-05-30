9786d6cd574443ac2c4bfd273d1c2f2a84af3a5d
Yulia <yulia.gorelikova1993@gmail.com>
2025-05-30 16:13:43 +0300

    1 step

---
+++ Makefile
@@ -0,0 +1,14 @@
+build:
+	make -C app build
+
+test:
+	make -C app test
+
+checkstyleMain:
+	make -C app checkstyleMain
+
+run:
+	make -C app run
+
+report:
+	make -C app report
---
+++ app/src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package hexlet.code.app.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to Spring";
+    }
+}
