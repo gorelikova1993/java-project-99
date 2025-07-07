4d9af5c2f1e2159dfbba03975ef2a0da0822f193
Yulia <yulia.gorelikova1993@gmail.com>
2025-07-07 22:38:36 +0300

    fix render error

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 	implementation("org.postgresql:postgresql:42.7.4")
+	implementation("org.flywaydb:flyway-core:11.10.1")
+	implementation("org.flywaydb:flyway-database-postgresql:11.10.1")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
