170ea10ff93a6fe5dede26cea2781f917df1c26d
Yulia <yulia.gorelikova1993@gmail.com>
2025-09-04 14:19:58 +0300

    fix review

--- src/main/java/hexlet/code/controller/TaskController.java
+++ src/main/java/hexlet/code/controller/TaskController.java
@@ -20,10 +20,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.data.jpa.domain.Specification.allOf;
 
 import java.util.List;
 
